8fa2be8eea4b0a74f14dde417e365851efc98d72
usernamefanta <143836045+usernamefanta@users.noreply.github.com>
2023-10-15 12:11:58 +0300

    Update payment.service.ts

--- src/app/components/services/payment.service.ts
+++ src/app/components/services/payment.service.ts
@@ -1,1 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PaymentService {
+  private apiUrl = '/api/payments';
+
+  constructor(private http: HttpClient) {}
+
+  processPayment(orderDetails: any): Observable<any> {
+    return this.http.post<any>(this.apiUrl, orderDetails);
+  }
+}
 

d025108874a26db4004b6bc1cd2f95b644e8d298
usernamefanta <143836045+usernamefanta@users.noreply.github.com>
2023-10-15 15:09:49 +0300

    Update product.service.ts

--- src/app/components/services/product.service.ts
+++ src/app/components/services/product.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core';
+/*import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Product } from '../models/product.model';
@@ -13,6 +13,29 @@ export class ProductService {
 
   getProducts(): Observable<Product[]> {
     return this.http.get<Product[]>(this.apiUrl);
+  }
+}
+*/
+// /src/app/services/product.service.ts
+
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProductService {
+  // Example product data
+  private products = [
+    { id: 1, name: 'Product 1', price: 19.99, description: 'Description for Product 1' },
+    { id: 2, name: 'Product 2', price: 29.99, description: 'Description for Product 2' },
+    { id: 3, name: 'Product 3', price: 39.99, description: 'Description for Product 3' },
+    // Add more products as needed
+  ];
+
+  constructor() {}
+
+  getProducts() {
+    return this.products;
   }
 }
 

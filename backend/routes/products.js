c1069ebdcca8bb87abf7d5f767dbc0336b3683d2
usernamefanta <143836045+usernamefanta@users.noreply.github.com>
2023-10-15 11:23:02 +0300

    Create products.js
    This file handles requests related to products (like fetching a list of products).
    It communicates with the MongoDB database to retrieve product information.

---
+++ backend/routes/products.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const router = express.Router();
+const Product = require('../models/Product');
+
+router.get('/', async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;

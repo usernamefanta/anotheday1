c3dd84c8dc8b997f30f351857ed29bcebfe1bad1
usernamefanta <143836045+usernamefanta@users.noreply.github.com>
2023-10-15 15:11:57 +0300

    Update product-list.component.ts

--- src/app/components/product-list.component.ts
+++ src/app/components/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+/*import { Component, OnInit } from '@angular/core';
 import { ProductService } from '../services/product.service';
 
 @Component({
@@ -17,4 +17,24 @@ export class ProductListComponent implements OnInit {
     });
   }
 }
+*/
+// /src/app/components/product-list.component.ts
 
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from '../services/product.service';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+  products = [];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit(): void {
+    // Fetch products from the service
+    this.products = this.productService.getProducts();
+  }
+}

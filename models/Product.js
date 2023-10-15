0d09183e5e4b1a09fe26429a4a159a45b012fd53
usernamefanta <143836045+usernamefanta@users.noreply.github.com>
2023-10-15 11:56:34 +0300

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,1 +1,10 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  description: String,
+});
+
+module.exports = mongoose.model('Product', productSchema);
 
